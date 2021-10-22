eb762478e88d8041e8e171ca3a66f5e1879d50dc
Andrew Metcalfe <andrew@moonjupiter.co.uk>
2021-10-22 09:33:38 +0100

    Initial commit

---
+++ ConsoleApp1/MusicBrainzNugetExample1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Hqub.MusicBrainz.API;
+using Hqub.MusicBrainz.API.Entities;
+
+namespace ConsoleApp1
+{
+    /// <summary>
+    ///     This was the original example from the MusicBrainz nuget package, which i've tweaked to get the song names.
+    /// </summary>
+    public class MusicBrainzNugetExample1
+    {
+        public static async Task Run(MusicBrainzClient client)
+        {
+            await Search(client, "The Rolling Stones");
+        }
+
+        public static async Task Search(MusicBrainzClient client, string name)
+        {
+            // Search for an artist by name (limit to 20 matches).
+            var artists = await client.Artists.SearchAsync(name, 20);
+
+            Console.WriteLine("Total matches for '{0}': {1}", name, artists.Count);
+
+            // Count matches with score 100.
+            var count = artists.Items.Count(a => a.Score == 100);
+
+            Console.WriteLine("Exact matches for '{0}': {1}", name, count);
+
+            // By default, search results will be ordered by score, so to get the
+            // best match you could do artists.Items.First(). Sometimes this method
+            // won't work (example: search for 'U2').
+            // 
+            // If the search string is the exact name, it might be better to compare
+            // to that string or to order by similarity, like done here:
+
+            //var artist = artists.Items.OrderByDescending(a => Levenshtein.Similarity(a.Name, name)).First();
+            var artist = artists.Items.First();
+
+            // Get detailed information of the artist, including band-members and related urls.
+            var includes = new[] {"artist-rels", "recordings"};
+            artist = await client.Artists.GetAsync(artist.Id, "artist-rels", "url-rels", "releases", "recordings",
+                "works");
+
+            Console.WriteLine();
+            Console.WriteLine("Current band members of '{0}':", artist.Name);
+            Console.WriteLine();
+
+            // Band members are represented as artist-artist relationships. To filter relations,
+            // inspect "TargetType" and "Type" properties.
+            var members = artist.Relations.Where(r => r.TargetType == "artist" && r.Type.Contains("member"));
+
+            foreach (var relation in members.Where(r => !(bool) r.Ended))
+                Console.WriteLine("     {0}", relation.Artist.Name);
+
+            // Lyric are represented as artist-url relationships.
+            var lyrics = artist.Relations.Where(r => r.TargetType == "url" && r.Type == "lyrics");
+
+            if (lyrics.Count() > 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine("You can find lyrics for '{0}' at", artist.Name);
+                Console.WriteLine();
+
+                foreach (var relation in lyrics) Console.WriteLine("     {0}", relation.Url.Resource);
+            }
+
+            // Lyric are represented as artist-url relationships.
+            var songs = artist.Releases.Select(r => r.Title).Distinct();
+            var r = artist.Recordings;
+            var w = artist.Works;
+
+            if (songs.Count() > 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Found {songs.Count()} related releases...");
+                Console.WriteLine();
+
+                foreach (var song in songs) Console.WriteLine($"     {song}");
+            }
+        }
+
+        public static async Task<string[]> SearchAndReturnSongNames(MusicBrainzClient client, string name)
+        {
+            // Search for an artist by name (limit to 20 matches).
+            var artists = await client.Artists.SearchAsync(name, 20);
+
+            Console.WriteLine("Total matches for '{0}': {1}", name, artists.Count);
+
+            // Count matches with score 100.
+            var count = artists.Items.Count(a => a.Score == 100);
+
+            Console.WriteLine("Exact matches for '{0}': {1}", name, count);
+
+            // By default, search results will be ordered by score, so to get the
+            // best match you could do artists.Items.First(). Sometimes this method
+            // won't work (example: search for 'U2').
+            // 
+            // If the search string is the exact name, it might be better to compare
+            // to that string or to order by similarity, like done here:
+
+            //var artist = artists.Items.OrderByDescending(a => Levenshtein.Similarity(a.Name, name)).First();
+            var artist = artists.Items.First();
+
+            // Get detailed information of the artist, including band-members and related urls.
+            var includes = new[] {"artist-rels", "recordings"};
+            artist = await client.Artists.GetAsync(artist.Id, "artist-rels", "url-rels", "releases", "recordings",
+                "works");
+
+            ShowBandMembers(artist);
+            ShowAlternativeLyricResources(artist);
+
+            var songs = artist.Releases.Select(r => r.Title).Distinct().ToArray();
+
+            if (songs.Count() > 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Found {songs.Count()} related releases...");
+                Console.WriteLine();
+
+                foreach (var song in songs) Console.WriteLine($"     {song}");
+            }
+
+            return songs;
+        }
+
+        private static void ShowAlternativeLyricResources(Artist artist)
+        {
+            // Lyric are represented as artist-url relationships.
+            var lyrics = artist.Relations.Where(r => r.TargetType == "url" && r.Type == "lyrics");
+
+            if (lyrics.Count() > 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine("You can find lyrics for '{0}' at", artist.Name);
+                Console.WriteLine();
+
+                foreach (var relation in lyrics) Console.WriteLine("     {0}", relation.Url.Resource);
+            }
+        }
+
+        private static void ShowBandMembers(Artist artist)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Current band members of '{0}':", artist.Name);
+            Console.WriteLine();
+
+            // Band members are represented as artist-artist relationships. To filter relations,
+            // inspect "TargetType" and "Type" properties.
+            var members = artist.Relations.Where(r => r.TargetType == "artist" && r.Type.Contains("member"));
+
+            foreach (var relation in members.Where(r => !(bool) r.Ended))
+                Console.WriteLine("     {0}", relation.Artist.Name);
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Hqub.MusicBrainz.API;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Make sure that TLS 1.2 is available.
+            ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls12;
+
+            try
+            {
+                var task = RunExamples();
+
+                task.Wait();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static async Task RunExamples()
+        {
+            // Get path for local file cache.
+            //var location = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            var client = new MusicBrainzClient();
+            //{
+            //    Cache = new FileRequestCache(Path.Combine(location, "cache"))
+            //};
+
+            Header("Hello, please enter the name of a band or artist...");
+
+            var artistName = Console.ReadLine();
+
+            // Get the names of the songs from the nuget package
+            // I thought this was OK for a quick example, why reinvent the wheel?
+            // but as it turned out it wasn't as userfriendly  to use as i'd hoped!
+            var songs = await MusicBrainzNugetAdapation.GetSongNames(client, artistName);
+
+            using (var lyricClient = new HttpClient())
+            {
+                var songTotal = 0;
+                var wordTotal = 0;
+
+                Console.WriteLine($"Found {songs.Length} songs.");
+
+                foreach (var song in songs)
+                {
+                    var requestUri = $"https://api.lyrics.ovh/v1/{artistName}/{song}";
+                    var lyricResponse = await GetLyricResponse(lyricClient, requestUri);
+
+                    if (lyricResponse is null)
+                    {
+                        Console.WriteLine($"    {song} : no lyrics found");
+                    }
+                    else
+                    {
+                        //Console.WriteLine("");
+                        Console.WriteLine($"    {song} : has {lyricResponse.WordCount} words");
+                        //Console.WriteLine(lyricResponse.Lyrics);
+                        songTotal++;
+                        wordTotal += lyricResponse.WordCount;
+                    }
+                }
+
+                Console.WriteLine($"Average words per song is {wordTotal / songTotal}");
+            }
+        }
+
+        private static async Task<LyricResponse> GetLyricResponse(HttpClient client, string uri)
+        {
+            try
+            {
+                return await client.GetFromJsonAsync<LyricResponse>(uri);
+            }
+            catch (HttpRequestException ex1)
+            {
+                // lets hide the errors as the spoil the appearance of the output
+                //Console.WriteLine("An error occurred");
+            }
+            catch (NotSupportedException ex2)
+            {
+                // lets hide the errors as the spoil the appearance of the output
+                // Console.WriteLine("Content type is not supported."); 
+            }
+            catch (JsonException ex3)
+            {
+                // lets hide the errors as the spoil the appearance of the output
+                // Console.WriteLine("Invalid JSON"); 
+            }
+
+            return null;
+        }
+
+        private static void Header(string title)
+        {
+            var color = Console.ForegroundColor;
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+            Console.WriteLine();
+            Console.WriteLine(title);
+            Console.WriteLine();
+
+            Console.ForegroundColor = color;
+        }
+    }
+}
